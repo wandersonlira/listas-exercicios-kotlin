cdf1f23217071ab4109f69b6863b1ba0bd3c02fd
Wanderson Lira <wandersonlira.dev1@gmail.com>
2024-03-19 23:56:15 -0300

    :sparkles: feat: Resolvido exercicio '12' da lista_DIO

---
+++ src/lista_DIO/Exercicio_12.kt
@@ -0,0 +1,20 @@
+package lista_DIO
+
+/*
+12 - Faça um algoritmo que leia o valor de um produto e determine o valor que deve ser pago, conforme a escolha da forma de pagamento
+pelo comprador e imprima na tela o valor final do produto a ser pago. Utilize os códigos da tabela de condições de pagamento para efetuar o cálculo adequado.
+
+Tabela de Código de Condições de Pagamento
+
+1 - À Vista em Dinheiro ou Pix, recebe 15% de desconto
+2 - À Vista no cartão de crédito, recebe 10% de desconto
+3 - Parcelado no cartão em duas vezes, preço normal do produto sem juros
+4 - Parcelado no cartão em três vezes ou mais, preço normal do produto mais juros de 10%
+ */
+class Exercicio_12 {
+
+    fun lerProduto(valor: Double) {
+        
+    }
+
+}
